5ee11cbc4f303e081e74560be377e33c348db35a
Biin106 <glfn30832@Naver.com>
2024-10-25 18:06:38 +0900

    Update 양꼬치.java

--- 프로그래머스/0/120830. 양꼬치/양꼬치.java
+++ 프로그래머스/0/120830. 양꼬치/양꼬치.java
@@ -2,7 +2,7 @@ class Solution {
     public int solution(int n, int k) {
         int answer = 0;
         if(n<10) {answer = (12000*n) + (2000*k);}
-        else {answer = ((12000*n) + (2000*k))-((n/10)*2000);}
+        else {answer = ((12000*n) + (2000*k))-((n/10)*2000);} // 어차피 정수 나눗셈에서 몫이 0이면 뒤의 *2000도 0이므로 범위 필요없음
         return answer;
     }
 }
