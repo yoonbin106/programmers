28739a6109ead0a2edbaf6a08bf6857ad49805aa
Biin106 <glfn30832@Naver.com>
2024-10-25 17:51:41 +0900

    [level 0] Title: 양꼬치, Time: 0.03 ms, Memory: 71.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/120830. 양꼬치/양꼬치.java
@@ -0,0 +1,8 @@
+class Solution {
+    public int solution(int n, int k) {
+        int answer = 0;
+        if(n<10) {answer = (12000*n) + (2000*k);}
+        else {answer = ((12000*n) + (2000*k))-((n/10)*2000);}
+        return answer;
+    }
+}
