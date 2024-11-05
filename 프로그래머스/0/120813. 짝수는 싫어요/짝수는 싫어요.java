83662bf5afeb389d70f778bd2a2677e999c71074
Biin106 <glfn30832@Naver.com>
2024-11-05 14:40:47 +0900

    Update 짝수는 싫어요.java

--- 프로그래머스/0/120813. 짝수는 싫어요/짝수는 싫어요.java
+++ 프로그래머스/0/120813. 짝수는 싫어요/짝수는 싫어요.java
@@ -1,18 +1,14 @@
 class Solution {
     public int[] solution(int n) {
-        int size = (n + 1) / 2;  // 홀수 개수 계산
-        
-        int[] result = new int[size];  // 홀수를 담을 배열 생성
-
-        // 홀수를 찾고 배열에 저장
-        int index = 0;  // 결과 배열의 인덱스
+        int size = (n + 1) / 2; 
+        int[] result = new int[size]; 
+        int index = 0;  
         for (int i = 1; i <= n; i++) {
             if (i % 2 != 0) { 
                 result[index] = i; 
-                index++;  // 인덱스 증가
+                index++;
             }
         }
-        
-        return result;  // 결과 배열 반환
+        return result;
     }
 }
