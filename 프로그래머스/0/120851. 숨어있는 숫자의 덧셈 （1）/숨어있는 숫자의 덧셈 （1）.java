7d22cd9f4b507ae540dd3921b18852e70e79e1a2
Biin106 <glfn30832@Naver.com>
2024-11-08 20:37:48 +0900

    Update 숨어있는 숫자의 덧셈 （1）.java

--- 프로그래머스/0/120851. 숨어있는 숫자의 덧셈 （1）/숨어있는 숫자의 덧셈 （1）.java
+++ 프로그래머스/0/120851. 숨어있는 숫자의 덧셈 （1）/숨어있는 숫자의 덧셈 （1）.java
@@ -3,10 +3,10 @@ class Solution {
         int sum=0;
         
         for (int i=0;i<my_string.length();i++){
-            char c=my_string.charAt(i);
+            char c=my_string.charAt(i); //문자열 순회
             
-            if(Character.isDigit(c)){
-                sum += Character.getNumericValue(c);
+            if(Character.isDigit(c)){ //Charcter.isDigit() -> 숫인지 판단
+                sum += Character.getNumericValue(c); // 숫자일경우 숫자로 변환후 sum에 더함
             }
         }
         return sum;
